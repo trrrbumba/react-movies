128f35f476e6d1e3c15ff1c83d897ef757d65fb1
trrrbumba <badaro@list.ru>
2022-08-15 00:16:51 +0300

    Updates

---
+++ precache-manifest.2fa43e71fff9ff62bca2f449afe43c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e934bcd985c6de64c2f7",
+    "url": "/react-movies/static/css/main.2e966bc6.chunk.css"
+  },
+  {
+    "revision": "e934bcd985c6de64c2f7",
+    "url": "/react-movies/static/js/main.e934bcd9.chunk.js"
+  },
+  {
+    "revision": "0df596195c8b1a5b6683",
+    "url": "/react-movies/static/js/1.0df59619.chunk.js"
+  },
+  {
+    "revision": "6e0964696bb83cd24dee",
+    "url": "/react-movies/static/js/runtime~main.6e096469.js"
+  },
+  {
+    "revision": "ba3178e3e6175a53a32906d242643e07",
+    "url": "/react-movies/index.html"
+  }
+];
